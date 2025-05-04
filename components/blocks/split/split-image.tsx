05e6d2d89ec1ae9f064c8487b905f0d11bc58d46
zodakzach <zachary.cervenka@gmail.com>
2025-05-03 23:11:26 -0400

    fix split image

--- components/blocks/split/split-image.tsx
+++ components/blocks/split/split-image.tsx
@@ -18,7 +18,7 @@ export default function SplitImage({ image }: SplitImage) {
         placeholder={image?.asset?.metadata?.lqip ? "blur" : undefined}
         blurDataURL={image?.asset?.metadata?.lqip || ""}
         fill
-        className="object-cover"
+        className="object-cover object-top"
         sizes="(min-width: 1024px) 25vw, (min-width: 640px) 50vw, 100vw"
         quality={100}
       />
